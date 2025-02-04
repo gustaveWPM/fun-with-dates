7eadfd9c23ea0185901f784c731a950972097f1e
GustaveWPM <jgeorge@perso.be>
2025-02-04 20:35:14 +0100

    chore: minor changes

--- src/__tests__/formatAllDatesToUtc.test.ts
+++ src/__tests__/formatAllDatesToUtc.test.ts
@@ -46,7 +46,6 @@ const dataset = {
 describe('formatAllDatesToUtc [impl]', () => {
   it('should let the structure unchanged, given an empty structure', () => {
     const input = {};
-
     const output = formatAllDatesToUtc(input);
 
     expect(output).toStrictEqual(input);
@@ -60,17 +59,15 @@ describe('formatAllDatesToUtc [impl]', () => {
   });
 
   it('should replace all dates by formatted strings, given flat structure with dates', () => {
-    const data = dataset.flatStructureWithDates;
-
-    const output = formatAllDatesToUtc(data);
+    const input = dataset.flatStructureWithDates;
+    const output = formatAllDatesToUtc(input);
 
     expect(output).toMatchSnapshot();
   });
 
   it('should recursively replace all dates by formatted strings, given structure with dates & nestings', () => {
-    const data = dataset.deepStructureWithDates;
-
-    const output = formatAllDatesToUtc(data);
+    const input = dataset.deepStructureWithDates;
+    const output = formatAllDatesToUtc(input);
 
     expect(output).toMatchSnapshot();
   });
