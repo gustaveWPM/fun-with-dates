1ecc18d3ced863adef7ac5e4c1521fa730ede5aa
GustaveWPM <jgeorge@perso.be>
2025-02-04 20:27:13 +0100

    chore: olol

--- src/__tests__/formatAllDatesToUtc.test.ts
+++ src/__tests__/formatAllDatesToUtc.test.ts
@@ -1,8 +1,47 @@
-import { assertType, describe, expect, it } from 'vitest';
+import { expectTypeOf, describe, expect, it } from 'vitest';
 
 import type { DatesToStrings } from '../types/DatesToStrings';
 
 import { formatAllDatesToUtc } from '../formatAllDatesToUtc';
+
+const dataset = {
+  deepStructureWithDates: {
+    nested: {
+      deeplyNested: {
+        a: new Date('1998-02-01'),
+        b: 'foo',
+        c: 12
+      },
+
+      b: new Date('1998-02-01'),
+      a: 'foo'
+    },
+
+    a: new Date('1998-02-01'),
+    d: new Date('1998-01-02'),
+    b: 'foo',
+    c: 'bar'
+  },
+
+  flatStructureWithoutDates: {
+    f: () => {
+      console.log('lol');
+    },
+    // eslint-disable-next-line no-magic-numbers
+    c: BigInt(28),
+    e: undefined,
+    b: 'foo',
+    d: null,
+    a: 12
+  },
+
+  flatStructureWithDates: {
+    a: new Date('1998-02-01'),
+    d: new Date('1998-01-02'),
+    b: 'foo',
+    c: 'bar'
+  }
+} as const;
 
 describe('formatAllDatesToUtc [impl]', () => {
   it('should let the structure unchanged, given an empty structure', () => {
@@ -14,52 +53,22 @@ describe('formatAllDatesToUtc [impl]', () => {
   });
 
   it('should let the structure unchanged, given a structure without dates', () => {
-    const input = {
-      // eslint-disable-next-line no-magic-numbers
-      c: BigInt(28),
-      e: undefined,
-      f: () => {},
-      b: 'foo',
-      d: null,
-      a: 12
-    };
-
+    const input = dataset.flatStructureWithoutDates;
     const output = formatAllDatesToUtc(input);
 
     expect(output).toStrictEqual(input);
   });
 
-  it('should replace all dates by formatted strings, given flat structure', () => {
-    const data = {
-      a: new Date('1998-02-01'),
-      d: new Date('1998-01-02'),
-      b: 'foo',
-      c: 'bar'
-    };
+  it('should replace all dates by formatted strings, given flat structure with dates', () => {
+    const data = dataset.flatStructureWithDates;
 
     const output = formatAllDatesToUtc(data);
 
     expect(output).toMatchSnapshot();
   });
 
-  it('should recursively replace all dates by formatted strings, given structure with nestings', () => {
-    const data = {
-      nested: {
-        deeplyNested: {
-          a: new Date('1998-02-01'),
-          b: 'foo',
-          c: 12
-        },
-
-        b: new Date('1998-02-01'),
-        a: 'foo'
-      },
-
-      a: new Date('1998-02-01'),
-      d: new Date('1998-01-02'),
-      b: 'foo',
-      c: 'bar'
-    };
+  it('should recursively replace all dates by formatted strings, given structure with dates & nestings', () => {
+    const data = dataset.deepStructureWithDates;
 
     const output = formatAllDatesToUtc(data);
 
@@ -68,14 +77,82 @@ describe('formatAllDatesToUtc [impl]', () => {
 });
 
 describe('formatAllDatesToUtc [typing]', () => {
-  it('should _, given _', () => {
-    const _ = {} as DatesToStrings<{
-      'hello.world': 'Hello World';
-      hello: 'Hello';
-    }>;
+  it('should let the type unchanged, given an empty structure type', () => {
+    type TInput = {};
+    type TOutput = DatesToStrings<TInput>;
 
-    assertType<'hello.world' | 'hello'>(_);
+    expectTypeOf<TOutput>().toEqualTypeOf<TInput>();
+  });
 
-    expect(true).toBe(true);
+  it('should let the type unchanged, given a structure without dates', () => {
+    type TInput = typeof dataset.flatStructureWithoutDates;
+    type TOutput = DatesToStrings<TInput>;
+
+    expectTypeOf<TOutput>().toEqualTypeOf<TInput>();
+  });
+
+  it('should replace all dates by strings, given flat structure with dates', () => {
+    type TInput = typeof dataset.flatStructureWithDates;
+    type TOutput = DatesToStrings<TInput>;
+
+    expectTypeOf<TOutput>().toEqualTypeOf<{
+      readonly a: string;
+      readonly d: string;
+      readonly b: 'foo';
+      readonly c: 'bar';
+    }>();
+  });
+
+  it('should recursively replace all dates by formatted strings, given structure with dates & nestings', () => {
+    type TInput = typeof dataset.deepStructureWithDates;
+    type TOutput = DatesToStrings<TInput>;
+
+    expectTypeOf<TOutput>().toEqualTypeOf<{
+      readonly nested: {
+        readonly deeplyNested: {
+          readonly a: string;
+          readonly b: 'foo';
+          // eslint-disable-next-line no-magic-numbers
+          readonly c: 12;
+        };
+        readonly b: string;
+        readonly a: 'foo';
+      };
+
+      readonly a: string;
+      readonly d: string;
+      readonly b: 'foo';
+      readonly c: 'bar';
+    }>();
+  });
+
+  it('should distribute optionals, given a structure with optional fields type & types unions', () => {
+    type TInput = {
+      a?: {
+        optionalDateOrString?: string | Date;
+        dateOrString: string | Date;
+        string: string;
+        date: Date;
+      };
+
+      optionalDateOrStringOrNull?: string | Date | null;
+      number: number;
+      date: Date;
+    };
+
+    type TOutput = DatesToStrings<TInput>;
+
+    expectTypeOf<TOutput>().toEqualTypeOf<{
+      a?: {
+        optionalDateOrString?: string;
+        dateOrString: string;
+        string: string;
+        date: string;
+      };
+
+      optionalDateOrStringOrNull?: string | null;
+      number: number;
+      date: string;
+    }>();
   });
 });
--- src/types/DatesToStrings.ts
+++ src/types/DatesToStrings.ts
@@ -2,4 +2,4 @@ type _DistributeDatesToStrings<T> = {
   [K in keyof T]: DatesToStrings<T[K]>;
 };
 
-export type DatesToStrings<T> = T extends Date ? string : T extends object ? _DistributeDatesToStrings<T> : T;
+export type DatesToStrings<T> = T extends Function ? T : T extends Date ? string : T extends object ? _DistributeDatesToStrings<T> : T;
