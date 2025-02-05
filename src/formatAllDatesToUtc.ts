e8ca3c2a6a3e91728b03f0b235988d632237ab6e
GustaveWPM <jgeorge@perso.be>
2025-02-05 04:07:51 +0100

    chore: minor changes

--- src/formatAllDatesToUtc.ts
+++ src/formatAllDatesToUtc.ts
@@ -2,10 +2,10 @@ import type { DatesToStrings } from './types/DatesToStrings';
 
 import dayjs from './modules/dayjs';
 
-const formatUtcDate = (d: Date): string => dayjs.utc(d).local().format();
+const toFormattedUtcDate = (d: Date): string => dayjs.utc(d).local().format();
 
 function formatEntry<Input extends object>(k: keyof Input, v: Input[typeof k]) {
-  if (v instanceof Date) return [k, formatUtcDate(v)];
+  if (v instanceof Date) return [k, toFormattedUtcDate(v)];
   if (v && typeof v === 'object') return [k, formatAllDatesToUtc(v)];
   return [k, v];
 }
